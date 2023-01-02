ad4355bbadfa031bf09c8e6d59ba7e5edb250742
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2023-01-02 21:26:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Budget-planner
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hfhvmr)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="mt-3">My Budget Planner</h1>
+    </div>
+  );
+}

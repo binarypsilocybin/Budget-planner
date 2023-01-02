1e850653d6c294bf4d895144a4819fe97e24f7ca
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2023-01-02 21:27:26 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <div className="container">
       <h1 className="mt-3">My Budget Planner</h1>
+      <div className="row mt-3"></div>
     </div>
   );
 }

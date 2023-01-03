b9f52bd8e26799fd261a1a5317fa2cc8d2f7f734
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2023-01-03 11:27:31 +0000

    1 file modified

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,14 +7,14 @@ const ExpenseList = () => {
     { id: 1231232, name: 'Holiday', cost: 100 },
     { id: 1231232, name: 'Transportation', cost: 60 },
     { id: 1231232, name: 'Fuel', cost: 75 },
-    { id: 1231232, Pname: 'Child Care', cost: 500 },
+    { id: 1231232, name: 'Child Care', cost: 500 },
   ];
 
   return (
-    <ul>
-      {expenses.map((expense) => {
-        <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />;
-      })}
+    <ul className="list-group">
+      {expenses.map((expense) => (
+        <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
+      ))}
     </ul>
   );
 };
